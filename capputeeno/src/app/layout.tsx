1884a1a7b024bb0dd4f6834231ba3cb9292c66dc
Guilherme Xavier <xguilherme1@gmail.com>
2023-06-11 20:00:20 -0300

    feat: creating filter context

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import { Saira } from 'next/font/google'
 import React from 'react'
+import { FilterContextProvider } from '@/contexts/FilterContext'
 
 const saira = Saira({
   weight: ['300', '400', '500', '600'],
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={saira.className}>
-        <Header />
-        {children}
+        <FilterContextProvider>
+          <Header />
+          {children}
+        </FilterContextProvider>
       </body>
     </html>
   )
---
+++ capputeeno/src/components/FilterByType.tsx
@@ -0,0 +1,58 @@
+import useFilter from '@/hooks/useFilter'
+import { FilterType } from '@/types/filterTypes'
+import styled from 'styled-components'
+
+interface FilterItemProps {
+  selected: boolean
+}
+
+const FilterList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+`
+
+const FilterItem = styled.li<FilterItemProps>`
+  font-family: inherit;
+  font-size: 16px;
+  font-weight: ${(props) => (props.selected ? '600' : '400')};
+  line-height: 22px;
+  text-align: center;
+  text-transform: uppercase;
+  cursor: pointer;
+  color: var(--text-dark);
+  border-bottom: ${(props) =>
+    props.selected ? '4px solid var(--orange-low)' : ''};
+`
+const FilterByType = () => {
+  const { type, setType } = useFilter()
+
+  const handleChangeType = (value: FilterType) => {
+    setType(value)
+  }
+  return (
+    <FilterList>
+      <FilterItem
+        selected={type === FilterType.ALL}
+        onClick={() => handleChangeType(FilterType.ALL)}
+      >
+        Todos os produtos
+      </FilterItem>
+      <FilterItem
+        selected={type === FilterType.SHIRT}
+        onClick={() => handleChangeType(FilterType.SHIRT)}
+      >
+        Camisetas
+      </FilterItem>
+      <FilterItem
+        selected={type === FilterType.MUG}
+        onClick={() => handleChangeType(FilterType.MUG)}
+      >
+        Canecas
+      </FilterItem>
+    </FilterList>
+  )
+}
+
+export default FilterByType
