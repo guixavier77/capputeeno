afe6204691e772a45bf498192d887bf82f6a8834
Guilherme Xavier <xguilherme1@gmail.com>
2023-06-14 22:03:59 -0300

    fix: local storage bugfix

--- capputeeno/src/components/FilterByPriority.tsx
+++ capputeeno/src/components/FilterByPriority.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import ArrowIcon from './ArrowIcon'
+import ArrowIcon from './icons/ArrowIcon'
 import { useState } from 'react'
 import useFilter from '@/hooks/useFilter'
 import { PriorityTypes } from '@/types/PriorityTypes'
